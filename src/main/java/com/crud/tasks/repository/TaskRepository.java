b14f6f91a9551e60a64e3bf2d3c4e4d32230d6bd
piotr-michon <piotr.michon01@gmail.com>
2019-05-26 00:13:53 +0200

    19.3 Commit

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -13,6 +13,9 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     Task save(Task task);
 
-//    @Override
-//    Optional<Task> findById(Long id);
+    @Override
+    Optional<Task> findById(Long id);
+
+    @Override
+    void deleteById(Long id);
 }
