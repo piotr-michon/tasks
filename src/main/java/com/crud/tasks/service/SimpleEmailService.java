fefce08b150883463b1a32bcac3fca53135c8074
piotr-michon <piotr.michon01@gmail.com>
2019-06-23 15:54:55 +0200

    23.4 Commit

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -32,9 +32,8 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (mail.getToCc() != null && !mail.getToCc().equals("")) {
+        if (mail.getToCc() != null && !(mail.getToCc().equals(""))) {
             mailMessage.setCc(mail.getToCc());
-            return mailMessage;
         }
         return mailMessage;
     }
